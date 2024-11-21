9d760b59e18d612c9f7e48e83d4da8d568679905
pedenys <pierreemmanuel.denys@gmail.com>
2024-11-21 10:23:27 +0100

    feat: customize font-size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,15 @@ export default {
   content: [
     "{routes,islands,components}/**/*.{ts,tsx,js,jsx}",
   ],
+  theme: {
+    fontSize: {
+      sm: "1.2rem",
+      base: "1.6rem",
+      xl: "2rem",
+      "2xl": "2.4rem",
+      "3xl": "3rem",
+      "4xl": "3.6rem",
+      "5xl": "5rem",
+    },
+  },
 } satisfies Config;
